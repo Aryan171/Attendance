ddcef877d48382ec03926323be029ca2caabe5b3
Aryan171 <62366692+Aryan171@users.noreply.github.com>
2025-06-16 11:38:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/attendance/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.attendance
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.attendance.ui.theme.AttendanceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AttendanceTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(
+    modifier: Modifier = Modifier,
+    name: String
+) {
+    Text(name, modifier = modifier)
+}
